85fa2234f52d09853f7a54d502b54ce8e70b4740
Balintataw J <balintataw2@gmail.com>
2019-06-14 11:52:36 -0700

    new build

---
+++ public/build/precache-manifest.82a207457347aaa3c1e70d772f783816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e199c599a38a68e9e71",
+    "url": "/static/js/main.0e199c59.chunk.js"
+  },
+  {
+    "revision": "dd70f819f9592bc891c5",
+    "url": "/static/css/1.e73e6bb3.chunk.css"
+  },
+  {
+    "revision": "dd70f819f9592bc891c5",
+    "url": "/static/js/1.dd70f819.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12f0c96d5e24af316c2eb04e7e43e8d5",
+    "url": "/index.html"
+  }
+];
